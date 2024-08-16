3cd1eb72cca825d100e2f6816d4a0fe2d95f35e2
igorjizn <killingasyringe@gmail.com>
2024-08-16 17:09:39 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,29 @@
+public class Cat {
+    private double speed;
+    private String name;
+
+    public Cat() {
+
+    }
+
+    public Cat(double speed, String name) {
+        this.speed = speed;
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
